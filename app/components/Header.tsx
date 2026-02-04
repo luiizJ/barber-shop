4589f9e68e4c407e345d1593d93f75d806d7ed78
Luiz Janampa <luiixeverson@gmail.com>
2026-02-03 22:25:08 -0300

    feat(uix): new logo in header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "./ui/card"
 import Link from "next/link"
 import Image from "next/image"
 import Sidebar from "./Sidebar"
-import { MenuIcon } from "lucide-react"
+import { MenuIcon, Scissors } from "lucide-react"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import { Button } from "./ui/button"
 
@@ -11,14 +11,28 @@ const Header = async () => {
     <header>
       <Card className="">
         <CardContent className="flex flex-row items-center justify-between p-3">
-          <Link href="/">
-            <Image
+          <Button asChild className="bg-transparent">
+            <Link href="/">
+              {/* <Image
               alt="Agenda barber"
               src="/img0.png"
               height={18}
               width={120}
-            />
-          </Link>
+            /> */}
+              <div className="flex items-center gap-2">
+                <div className="bg-primary shadow-primary/20 flex h-8 w-8 items-center justify-center rounded-lg shadow-lg">
+                  <Scissors className="h-5 w-5 text-white" />
+                </div>
+                <h1 className="text-xl font-extrabold tracking-tighter">
+                  <span className="bg-linear-to-r from-white to-gray-400 bg-clip-text text-transparent">
+                    Agdly
+                  </span>
+                  <span className="text-primary italic">flow</span>
+                </h1>
+              </div>
+            </Link>
+          </Button>
+
           <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline">
